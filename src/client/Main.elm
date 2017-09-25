e739b2ff9289378dfe039893d31f5696159410da
Daniel Ström <D@nielstrom.se>
2017-09-25 16:38:37 +0200

    Use Html.program

--- src/client/Main.elm
+++ src/client/Main.elm
@@ -1,7 +1,47 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (..)
 
-main : Html.Html msg
+main : Program Never Model Msg
 main =
-    text "Hello!"
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+    }
+
+
+type alias Model = Int
+
+
+type Msg
+    = Asdf
+    | Fdsa
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Asdf ->
+            (model, Cmd.none)
+
+        Fdsa ->
+            (model, Cmd.none)
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text "Hello!"
+        ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+init : (Model, Cmd Msg)
+init = 
+    (0, Cmd.none)
