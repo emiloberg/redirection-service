43988d015c4b29ec240fa8a540190501ea32f734
Daniel Ström <D@nielstrom.se>
2017-09-25 17:49:26 +0200

    Add table

--- src/client/Main.elm
+++ src/client/Main.elm
@@ -1,6 +1,10 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Date exposing (Date)
+import Date.Extra.Format as Format exposing (format)
+import Date.Extra.Config.Config_en_us exposing (config)
+
 
 main : Program Never Model Msg
 main =
@@ -8,11 +12,23 @@ main =
         { init = init
         , view = view
         , update = update
-        , subscriptions = subscriptions
+        , subscriptions = always Sub.none
+        }
+
+
+type alias Rule =
+    { from : String
+    , to : String
+    , why : String
+    , who : String
+    , created : Date
+    , updated : Date
     }
 
 
-type alias Model = Int
+type alias Model =
+    { rules : List Rule
+    }
 
 
 type Msg
@@ -20,28 +36,67 @@ type Msg
     | Fdsa
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Asdf ->
-            (model, Cmd.none)
+            ( model, Cmd.none )
 
         Fdsa ->
-            (model, Cmd.none)
+            ( model, Cmd.none )
+
+
+dateToString : Date -> String
+dateToString date =
+    format config config.format.dateTime date
+
+
+
+-- String.join "-"
+--     [ toString (Date.year date)
+--     , toString (Date.month date)
+--     , toString (Date.day date)
+--     ]
+
+
+ruleToRow : Rule -> Html msg
+ruleToRow rule =
+    tr []
+        [ td [] [ text rule.from ]
+        , td [] [ text rule.to ]
+        , td [] [ text rule.why ]
+        , td [] [ text rule.who ]
+        , td [] [ text (dateToString rule.created) ]
+        , td [] [ text (dateToString rule.updated) ]
+        ]
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ text "Hello!"
+    table []
+        [ thead []
+            [ tr []
+                [ th [] [ text "From" ]
+                , th [] [ text "To" ]
+                , th [] [ text "Why" ]
+                , th [] [ text "Who" ]
+                , th [] [ text "Created" ]
+                , th [] [ text "Updated" ]
+                ]
+            ]
+        , tbody []
+            (List.map
+                ruleToRow
+                model.rules
+            )
         ]
 
 
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
-
-
-init : (Model, Cmd Msg)
-init = 
-    (0, Cmd.none)
+init : ( Model, Cmd Msg )
+init =
+    ( Model
+        [ Rule "/" "/404" "Because" "me" (Date.fromString "2016-01-01" |> Result.withDefault (Date.fromTime 0)) (Date.fromString "2016-01-01" |> Result.withDefault (Date.fromTime 0))
+        , Rule "/" "/404" "asdf" "you" (Date.fromString "2016-01-01" |> Result.withDefault (Date.fromTime 0)) (Date.fromString "2016-01-01" |> Result.withDefault (Date.fromTime 0))
+        ]
+    , Cmd.none
+    )
