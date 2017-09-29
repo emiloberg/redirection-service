4d817fdd3d57758f38f115befb74983cc5bab61b
Daniel Ström <D@nielstrom.se>
2017-09-29 12:03:55 +0200

    WIP: authentication without database

--- src/server/server.js
+++ src/server/server.js
@@ -1,50 +1,97 @@
 const fs = require("fs")
 const path = require("path")
 const http = require("http")
+const passport = require("koa-passport")
+const morgan = require("koa-morgan")
+const session = require("koa-session")
+const GoogleStrategy = require("passport-google-oauth20").Strategy
 const { postgraphql } = require("postgraphql")
-// const auth = require("http-auth")
+const bodyParser = require("koa-bodyparser")
 const CONFIG = require("./config")
+const Koa = require("koa")
+const Router = require("koa-router")
+
+const app = new Koa()
+const router = new Router()
 
 const indexHtml = fs.readFileSync(path.join(__dirname, "../client/index.html")).toString()
 const elmJs = fs.readFileSync(path.join(__dirname, "../../dist/client.js")).toString()
 // const mainCss = fs.readFileSync("./src/client/main.css").toString()
+
 const graph = postgraphql(CONFIG.DATABASE_URL, CONFIG.DATABASE_SCHEMA, CONFIG.POSTGRAPHQL_OPTIONS)
 
-// const basic = auth.basic(
-//   {
-//     realm: "iZettle Support Devices"
-//   },
-//   (username, password, callback) => {
-//     callback(username === "wwwtest" && password === "qwer1234")
-//   }
-// )
-
-http
-  .createServer(
-    /*basic,*/ (req, res) => {
-      switch (req.url) {
-        case "/":
-          res.writeHead(200)
-          res.end(indexHtml)
-          break
-
-        case "/client.js":
-          res.writeHead(200)
-          res.end(elmJs)
-          break
-
-        // case "/main.css":
-        //   res.setHeader("content-type", "text/css")
-        //   res.writeHead(200)
-        //   res.end(mainCss)
-        //   break
-
-        default:
-          graph(req, res)
-          break
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: CONFIG.GOOGLE_CLIENT_ID,
+      clientSecret: CONFIG.GOOGLE_CLIENT_SECRET,
+      callbackURL: CONFIG.GOOGLE_REDIRECT_URI
+    },
+    function(accessToken, refreshToken, profile, done) {
+      if (!profile.emails.some(email => email.value.endsWith("izettle.com"))) {
+        done(new Error("Not the correct domain."))
+        return
       }
+
+      done(null, profile)
+      // User.findOrCreate({ googleId: profile.id }, function(err, user) {
+      //   return done(err, user)
+      // })
     }
   )
-  .listen(CONFIG.APP_PORT, "0.0.0.0", 511, event => {
-    console.log(`Listening on port ${CONFIG.APP_PORT}.`)
-  })
+)
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id)
+})
+
+passport.deserializeUser(function(id, done) {
+  done(null, id)
+  // User.findById(id, function(err, user) {
+  //   done(err, user)
+  // })
+})
+
+router.get("/client.js", async ctx => {
+  ctx.body = elmJs
+})
+
+// router.get("/main.css", (req, res) => {
+//   res.setHeader("content-type", "text/css")
+//   res.writeHead(200)
+//   res.end(mainCss)
+// })
+
+router.get("/login", passport.authenticate("google", { scope: ["profile", "email"] }))
+
+router.get(
+  "/login/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login",
+    scope: ["profile", "email"]
+  }),
+  async ctx => {
+    console.log("aSDFASDF")
+    // Successful authentication, redirect home.
+    ctx.redirect("/")
+  }
+)
+
+router.get("/", async ctx => {
+  ctx.body = indexHtml
+})
+
+app.use(session({}, app))
+app.keys = [CONFIG.SESSION_SECRET]
+
+app
+  .use(passport.initialize())
+  .use(passport.session())
+  .use(morgan("dev"))
+  .use(graph)
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+http.createServer(app.callback()).listen(CONFIG.APP_PORT, "0.0.0.0", 511, event => {
+  console.log(`Listening on port ${CONFIG.APP_PORT}.`)
+})
