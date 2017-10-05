237460d0b971075284b16318d3e388a047cdb6b5
Daniel Ström <D@nielstrom.se>
2017-10-05 08:35:10 +0200

    Extract routes to separate module

---
+++ src/server/routes.js
@@ -0,0 +1,47 @@
+const fs = require("fs")
+const path = require("path")
+const Router = require("koa-router")
+const passport = require("koa-passport")
+const CONFIG = require("./config")
+const router = new Router()
+const db = require("./database")
+
+const indexHtml = fs.readFileSync(path.join(__dirname, "../client/index.html")).toString()
+const elmJs = fs.readFileSync(path.join(__dirname, "../../dist/client.js")).toString()
+
+router.get("/client.js", async ctx => {
+  ctx.body = elmJs
+})
+
+router.get(CONFIG.LOGIN_URL, passport.authenticate("google", { scope: ["profile", "email"] }))
+
+router.get(
+  CONFIG.LOGIN_CALLBACK_URL,
+  passport.authenticate("google", {
+    failureRedirect: CONFIG.LOGIN_URL,
+    scope: ["profile", "email"]
+  }),
+  async ctx => ctx.redirect("/")
+)
+
+router.get("/", async ctx => {
+  ctx.body = indexHtml
+})
+
+router.put("/rules/:id", async ctx => {
+  ctx.body = await db.updateRule(ctx.params.id, ctx.request.body, ctx.state.user.emails[0].value)
+})
+
+router.get("/rules", async ctx => {
+  ctx.body = await db.getAllRules()
+})
+
+router.post("/rules", async ctx => {
+  ctx.body = await db.createRule(ctx.request.body, ctx.state.user.emails[0].value)
+})
+
+router.delete("/rules/:id", async ctx => {
+  ctx.body = await db.deleteRule(ctx.params.id)
+})
+
+module.exports = router
--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,3 @@
-const fs = require("fs")
-const path = require("path")
 const http = require("http")
 const passport = require("koa-passport")
 const morgan = require("koa-morgan")
@@ -8,19 +6,10 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy
 const bodyParser = require("koa-bodyparser")
 const CONFIG = require("./config")
 const Koa = require("koa")
-const Router = require("koa-router")
 
+const router = require("./routes")
 const app = new Koa()
-const router = new Router()
-
-const indexHtml = fs.readFileSync(path.join(__dirname, "../client/index.html")).toString()
-const elmJs = fs.readFileSync(path.join(__dirname, "../../dist/client.js")).toString()
-// const mainCss = fs.readFileSync("./src/client/main.css").toString()
-
-const LOGIN_URL = "/login"
-const LOGIN_CALLBACK_URL = "/login/callback"
-
-const db = require("./database")
+app.keys = [CONFIG.SESSION_SECRET]
 
 passport.use(
   new GoogleStrategy(
@@ -29,7 +18,7 @@ passport.use(
       clientSecret: CONFIG.GOOGLE_CLIENT_SECRET,
       callbackURL: CONFIG.GOOGLE_REDIRECT_URI
     },
-    function(accessToken, refreshToken, profile, done) {
+    (accessToken, refreshToken, profile, done) => {
       if (!profile.emails.some(email => email.value.endsWith("izettle.com"))) {
         done(new Error("Not the correct domain."))
         return
@@ -40,67 +29,23 @@ passport.use(
   )
 )
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser((user, done) => {
   done(null, user)
 })
 
 passport.deserializeUser((profile, done) => done(null, profile))
 
-router.get("/client.js", async ctx => {
-  ctx.body = elmJs
-})
-
-// router.get("/main.css", (req, res) => {
-//   res.setHeader("content-type", "text/css")
-//   res.writeHead(200)
-//   res.end(mainCss)
-// })
-
-router.get(LOGIN_URL, passport.authenticate("google", { scope: ["profile", "email"] }))
-
-router.get(
-  LOGIN_CALLBACK_URL,
-  passport.authenticate("google", {
-    failureRedirect: LOGIN_URL,
-    scope: ["profile", "email"]
-  }),
-  async ctx => ctx.redirect("/")
-)
-
-router.get("/", async ctx => {
-  ctx.body = indexHtml
-})
-
-router.put("/rules/:id", async ctx => {
-  ctx.body = await db.updateRule(ctx.params.id, ctx.request.body, ctx.state.user.emails[0].value)
-})
-
-router.get("/rules", async ctx => {
-  ctx.body = await db.getAllRules()
-})
-
-router.post("/rules", async ctx => {
-  ctx.body = await db.createRule(ctx.request.body, ctx.state.user.emails[0].value)
-})
-
-router.delete("/rules/:id", async ctx => {
-  ctx.body = await db.deleteRule(ctx.params.id)
-})
-
-app.use(session({}, app))
-app.keys = [CONFIG.SESSION_SECRET]
-
-app.use(passport.initialize()).use(passport.session())
-
-app.use(async (ctx, next) => {
-  if (ctx.isAuthenticated() || ctx.url.startsWith(LOGIN_URL)) {
-    await next()
-  } else {
-    await ctx.redirect(LOGIN_URL)
-  }
-})
-
 app
+  .use(session({}, app))
+  .use(passport.initialize())
+  .use(passport.session())
+  .use(async (ctx, next) => {
+    if (ctx.isAuthenticated() || ctx.url.startsWith(CONFIG.LOGIN_URL)) {
+      await next()
+    } else {
+      await ctx.redirect(CONFIG.LOGIN_URL)
+    }
+  })
   .use(bodyParser())
   .use(morgan("dev"))
   .use(router.routes())
